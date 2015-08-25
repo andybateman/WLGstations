ba000ce1d63a72c93fdb9bdcdc552adb4c451d30
Andy Bateman <andy@andybateman.com>
2015-08-26 09:31:58 +1200

    Adjusted structure to account for multiple trains between stations

--- routes.js
+++ routes.js
@@ -1,14 +1,16 @@
 routes =
-[{
+{
 	"WRL":[
 	{
 		"routeID":"1",
+		"routeLocationID":"1",
 		"StationID":"1",
 		"Time":"0001",
 	},
 	{
-		"routeID":"2",
+		"routeID":"1",
+		"routeLocationID":"2",
 		"StationID":"48",
 		"Time":"2359",
 	}]
-}]
+}
