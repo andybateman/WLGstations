d3cf0ce433897d15fba7375b9e074c25b71d952c
Andy Bateman <andy@andybateman.com>
2015-08-26 12:39:15 +1200

    Replaced array for object and code to mat h

--- stations.js
+++ stations.js
@@ -1,385 +1,387 @@
 stations =
-[{
-"StationID":"1",
-"Station":"Wellington Station",
-"ShortCode":"WELL",
-"Latitude":"-41.27907882",
-"Longitude":"174.7804584",
-"Elevation":"5"
-},
 {
-"StationID":"2",
-"Station":"Crofton Downs Station",
-"ShortCode":"CROF",
-"Latitude":"-41.25550412",
-"Longitude":"174.7665992",
-"Elevation":"65.821"
-},
-{
-"StationID":"3",
-"Station":"Ngaio Station",
-"ShortCode":"NGAI",
-"Latitude":"-41.25106116",
-"Longitude":"174.7717944",
-"Elevation":"91.359"
-},
-{
-"StationID":"4",
-"Station":"Simla Crescent Station",
-"ShortCode":"SIML",
-"Latitude":"-41.24685851",
-"Longitude":"174.7842935",
-"Elevation":"124.25"
-},
-{
-"StationID":"5",
-"Station":"Awarua Street Station",
-"ShortCode":"AWAR",
-"Latitude":"-41.24626157",
-"Longitude":"174.7755388",
-"Elevation":"116.927"
-},
-{
-"StationID":"6",
-"Station":"Box Hill Station",
-"ShortCode":"BOX",
-"Latitude":"-41.24610024",
-"Longitude":"174.7888747",
-"Elevation":"138.105"
-},
-{
-"StationID":"7",
-"Station":"Khandallah Station",
-"ShortCode":"KHAN",
-"Latitude":"-41.24289762",
-"Longitude":"174.7933272",
-"Elevation":"148"
-},
-{
-"StationID":"8",
-"Station":"Ngauranga Station",
-"ShortCode":"NGAU",
-"Latitude":"-41.2490365",
-"Longitude":"174.8130254",
-"Elevation":"0"
-},
-{
-"StationID":"9",
-"Station":"Raroa Station",
-"ShortCode":"RARO",
-"Latitude":"-41.2321673",
-"Longitude":"174.8017708",
-"Elevation":"158"
-},
-{
-"StationID":"10",
-"Station":"Johnsonville Station",
-"ShortCode":"JOHN",
-"Latitude":"-41.22365441",
-"Longitude":"174.8047212",
-"Elevation":"144"
-},
-{
-"StationID":"11",
-"Station":"Petone Station",
-"ShortCode":"PETO",
-"Latitude":"-41.22189758",
-"Longitude":"174.8696394",
-"Elevation":"1.055"
-},
-{
-"StationID":"12",
-"Station":"Takapu Road Station",
-"ShortCode":"TAKA",
-"Latitude":"-41.18511871",
-"Longitude":"174.8281852",
-"Elevation":"40.407"
-},
-{
-"StationID":"13",
-"Station":"Ava Station",
-"ShortCode":"AVA",
-"Latitude":"-41.2197675",
-"Longitude":"174.892103",
-"Elevation":"0"
-},
-{
-"StationID":"14",
-"Station":"Western Hutt Station",
-"ShortCode":"WEST",
-"Latitude":"-41.21188136",
-"Longitude":"174.8900105",
-"Elevation":"8.052"
-},
-{
-"StationID":"15",
-"Station":"Redwood Station",
-"ShortCode":"REDW",
-"Latitude":"-41.17650279",
-"Longitude":"174.8269407",
-"Elevation":"36.998"
-},
-{
-"StationID":"16",
-"Station":"Woburn Station",
-"ShortCode":"WOBU",
-"Latitude":"-41.22109383",
-"Longitude":"174.911082",
-"Elevation":"1.613"
-},
-{
-"StationID":"17",
-"Station":"Tawa Station",
-"ShortCode":"TAWA",
-"Latitude":"-41.16916181",
-"Longitude":"174.828389",
-"Elevation":"33.376"
-},
-{
-"StationID":"18",
-"Station":"Melling Station",
-"ShortCode":"MELL",
-"Latitude":"-41.20372907",
-"Longitude":"174.9051114",
-"Elevation":"5.522"
-},
-{
-"StationID":"19",
-"Station":"Waterloo Station",
-"ShortCode":"WATE",
-"Latitude":"-41.21389102",
-"Longitude":"174.9211563",
-"Elevation":"0"
-},
-{
-"StationID":"20",
-"Station":"Linden Station",
-"ShortCode":"LIND",
-"Latitude":"-41.15887982",
-"Longitude":"174.8319081",
-"Elevation":"16.274"
-},
-{
-"StationID":"21",
-"Station":"Epuni Station",
-"ShortCode":"EPUN",
-"Latitude":"-41.20767216",
-"Longitude":"174.930013",
-"Elevation":"0"
-},
-{
-"StationID":"22",
-"Station":"Kenepuru Station",
-"ShortCode":"KENE",
-"Latitude":"-41.14970302",
-"Longitude":"174.8384742",
-"Elevation":"19"
-},
-{
-"StationID":"23",
-"Station":"Naenae Station",
-"ShortCode":"NAEN",
-"Latitude":"-41.1983367",
-"Longitude":"174.9453284",
-"Elevation":"0"
-},
-{
-"StationID":"24",
-"Station":"Porirua Station",
-"ShortCode":"PORI",
-"Latitude":"-41.1379878",
-"Longitude":"174.8433772",
-"Elevation":"13.023"
-},
-{
-"StationID":"25",
-"Station":"Wingate Station",
-"ShortCode":"WING",
-"Latitude":"-41.18899429",
-"Longitude":"174.9545387",
-"Elevation":"17"
-},
-{
-"StationID":"26",
-"Station":"Taita Station",
-"ShortCode":"TAIT",
-"Latitude":"-41.18014471",
-"Longitude":"174.9608151",
-"Elevation":"28.121"
-},
-{
-"StationID":"27",
-"Station":"Pomare Station",
-"ShortCode":"POMA",
-"Latitude":"-41.16958985",
-"Longitude":"174.9695591",
-"Elevation":"29.553"
-},
-{
-"StationID":"28",
-"Station":"Paremata Station",
-"ShortCode":"PARE",
-"Latitude":"-41.10604716",
-"Longitude":"174.8664764",
-"Elevation":"3"
-},
-{
-"StationID":"29",
-"Station":"Manor Park Station",
-"ShortCode":"MANO",
-"Latitude":"-41.15629494",
-"Longitude":"174.978979",
-"Elevation":"24"
-},
-{
-"StationID":"30",
-"Station":"Mana Station",
-"ShortCode":"MANA",
-"Latitude":"-41.09515696",
-"Longitude":"174.8681072",
-"Elevation":"6.148"
-},
-{
-"StationID":"31",
-"Station":"Plimmerton Station",
-"ShortCode":"PLIM",
-"Latitude":"-41.08409512",
-"Longitude":"174.8673025",
-"Elevation":"2"
-},
-{
-"StationID":"32",
-"Station":"Silverstream Station",
-"ShortCode":"SILV",
-"Latitude":"-41.14734399",
-"Longitude":"175.0102428",
-"Elevation":"40.258"
-},
-{
-"StationID":"33",
-"Station":"Heretaunga Station",
-"ShortCode":"HERE",
-"Latitude":"-41.14252871",
-"Longitude":"175.0256494",
-"Elevation":"48"
-},
-{
-"StationID":"34",
-"Station":"Trentham Station",
-"ShortCode":"TREN",
-"Latitude":"-41.13781812",
-"Longitude":"175.0384275",
-"Elevation":"18"
-},
-{
-"StationID":"35",
-"Station":"Pukerua Bay Station",
-"ShortCode":"PUKE",
-"Latitude":"-41.03842919",
-"Longitude":"174.8864535",
-"Elevation":"80"
-},
-{
-"StationID":"36",
-"Station":"Wallaceville Station",
-"ShortCode":"WALL",
-"Latitude":"-41.13050513",
-"Longitude":"175.0587908",
-"Elevation":"60.778"
-},
-{
-"StationID":"37",
-"Station":"Upper Hutt Station",
-"ShortCode":"UPPE",
-"Latitude":"-41.12620584",
-"Longitude":"175.0708178",
-"Elevation":"51"
-},
-{
-"StationID":"38",
-"Station":"Maymorn Station",
-"ShortCode":"MAYM",
-"Latitude":"-41.10822175",
-"Longitude":"175.1327232",
-"Elevation":"125.517"
-},
-{
-"StationID":"39",
-"Station":"Paekakariki Station",
-"ShortCode":"PAEK",
-"Latitude":"-40.98747572",
-"Longitude":"174.9539164",
-"Elevation":"6.559"
-},
-{
-"StationID":"40",
-"Station":"Paraparaumu Station",
-"ShortCode":"PARA",
-"Latitude":"-40.91595474",
-"Longitude":"175.0076134",
-"Elevation":"12.445"
-},
-{
-"StationID":"41",
-"Station":"Featherston Station",
-"ShortCode":"FEAT",
-"Latitude":"-41.11329682",
-"Longitude":"175.3302922",
-"Elevation":"45.134"
-},
-{
-"StationID":"42",
-"Station":"Waikanae Station",
-"ShortCode":"WAIK",
-"Latitude":"-40.87660559",
-"Longitude":"175.0660641",
-"Elevation":"31.182"
-},
-{
-"StationID":"43",
-"Station":"Woodside Station",
-"ShortCode":"WOOD",
-"Latitude":"-41.06730336",
-"Longitude":"175.4017033",
-"Elevation":"95"
-},
-{
-"StationID":"44",
-"Station":"Matarawa Station",
-"ShortCode":"MATA",
-"Latitude":"-41.0486963",
-"Longitude":"175.448524",
-"Elevation":"73"
-},
-{
-"StationID":"45",
-"Station":"Carterton Station",
-"ShortCode":"CART",
-"Latitude":"-41.02205478",
-"Longitude":"175.5232396",
-"Elevation":"80"
-},
-{
-"StationID":"46",
-"Station":"Solway Station",
-"ShortCode":"SOLW",
-"Latitude":"-40.95370223",
-"Longitude":"175.6248631",
-"Elevation":"123"
-},
-{
-"StationID":"47",
-"Station":"Renall Street Station",
-"ShortCode":"RENA",
-"Latitude":"-40.94744654",
-"Longitude":"175.6392827",
-"Elevation":"122"
-},
-{
-"StationID":"48",
-"Station":"Masterton Station",
-"ShortCode":"MAST",
-"Latitude":"-40.94031498",
-"Longitude":"175.6550755",
-"Elevation":"115"
-}];
+	1:{
+		"StationID":"1",
+		"Station":"Wellington Station",
+		"ShortCode":"WELL",
+		"Latitude":"-41.27907882",
+		"Longitude":"174.7804584",
+		"Elevation":"5"
+	},
+	2:{
+		"StationID":"2",
+		"Station":"Crofton Downs Station",
+		"ShortCode":"CROF",
+		"Latitude":"-41.25550412",
+		"Longitude":"174.7665992",
+		"Elevation":"65.821"
+	},
+	3:{
+		"StationID":"3",
+		"Station":"Ngaio Station",
+		"ShortCode":"NGAI",
+		"Latitude":"-41.25106116",
+		"Longitude":"174.7717944",
+		"Elevation":"91.359"
+	},
+	4:{
+		"StationID":"4",
+		"Station":"Simla Crescent Station",
+		"ShortCode":"SIML",
+		"Latitude":"-41.24685851",
+		"Longitude":"174.7842935",
+		"Elevation":"124.25"
+	},
+	5:{
+		"StationID":"5",
+		"Station":"Awarua Street Station",
+		"ShortCode":"AWAR",
+		"Latitude":"-41.24626157",
+		"Longitude":"174.7755388",
+		"Elevation":"116.927"
+	},
+	6:{
+		"StationID":"6",
+		"Station":"Box Hill Station",
+		"ShortCode":"BOX",
+		"Latitude":"-41.24610024",
+		"Longitude":"174.7888747",
+		"Elevation":"138.105"
+	},
+	7:{
+		"StationID":"7",
+		"Station":"Khandallah Station",
+		"ShortCode":"KHAN",
+		"Latitude":"-41.24289762",
+		"Longitude":"174.7933272",
+		"Elevation":"148"
+	},
+	8:{
+		"StationID":"8",
+		"Station":"Ngauranga Station",
+		"ShortCode":"NGAU",
+		"Latitude":"-41.2490365",
+		"Longitude":"174.8130254",
+		"Elevation":"0"
+	},
+	9:{
+		"StationID":"9",
+		"Station":"Raroa Station",
+		"ShortCode":"RARO",
+		"Latitude":"-41.2321673",
+		"Longitude":"174.8017708",
+		"Elevation":"158"
+	},
+	10:{
+		"StationID":"10",
+		"Station":"Johnsonville Station",
+		"ShortCode":"JOHN",
+		"Latitude":"-41.22365441",
+		"Longitude":"174.8047212",
+		"Elevation":"144"
+	},
+	11:{
+		"StationID":"11",
+		"Station":"Petone Station",
+		"ShortCode":"PETO",
+		"Latitude":"-41.22189758",
+		"Longitude":"174.8696394",
+		"Elevation":"1.055"
+	},
+	12:{
+		"StationID":"12",
+		"Station":"Takapu Road Station",
+		"ShortCode":"TAKA",
+		"Latitude":"-41.18511871",
+		"Longitude":"174.8281852",
+		"Elevation":"40.407"
+	},
+	13:{
+		"StationID":"13",
+		"Station":"Ava Station",
+		"ShortCode":"AVA",
+		"Latitude":"-41.2197675",
+		"Longitude":"174.892103",
+		"Elevation":"0"
+	},
+	14:{
+		"StationID":"14",
+		"Station":"Western Hutt Station",
+		"ShortCode":"WEST",
+		"Latitude":"-41.21188136",
+		"Longitude":"174.8900105",
+		"Elevation":"8.052"
+	},
+	15:{
+		"StationID":"15",
+		"Station":"Redwood Station",
+		"ShortCode":"REDW",
+		"Latitude":"-41.17650279",
+		"Longitude":"174.8269407",
+		"Elevation":"36.998"
+	},
+	16:{
+		"StationID":"16",
+		"Station":"Woburn Station",
+		"ShortCode":"WOBU",
+		"Latitude":"-41.22109383",
+		"Longitude":"174.911082",
+		"Elevation":"1.613"
+	},
+	17:{
+		"StationID":"17",
+		"Station":"Tawa Station",
+		"ShortCode":"TAWA",
+		"Latitude":"-41.16916181",
+		"Longitude":"174.828389",
+		"Elevation":"33.376"
+	},
+	18:{
+		"StationID":"18",
+		"Station":"Melling Station",
+		"ShortCode":"MELL",
+		"Latitude":"-41.20372907",
+		"Longitude":"174.9051114",
+		"Elevation":"5.522"
+	},
+	19:{
+		"StationID":"19",
+		"Station":"Waterloo Station",
+		"ShortCode":"WATE",
+		"Latitude":"-41.21389102",
+		"Longitude":"174.9211563",
+		"Elevation":"0"
+	},
+	20:{
+		"StationID":"20",
+		"Station":"Linden Station",
+		"ShortCode":"LIND",
+		"Latitude":"-41.15887982",
+		"Longitude":"174.8319081",
+		"Elevation":"16.274"
+	},
+	21:{
+		"StationID":"21",
+		"Station":"Epuni Station",
+		"ShortCode":"EPUN",
+		"Latitude":"-41.20767216",
+		"Longitude":"174.930013",
+		"Elevation":"0"
+	},
+	22:{
+		"StationID":"22",
+		"Station":"Kenepuru Station",
+		"ShortCode":"KENE",
+		"Latitude":"-41.14970302",
+		"Longitude":"174.8384742",
+		"Elevation":"19"
+	},
+	23:{
+		"StationID":"23",
+		"Station":"Naenae Station",
+		"ShortCode":"NAEN",
+		"Latitude":"-41.1983367",
+		"Longitude":"174.9453284",
+		"Elevation":"0"
+	},
+	24:{
+		"StationID":"24",
+		"Station":"Porirua Station",
+		"ShortCode":"PORI",
+		"Latitude":"-41.1379878",
+		"Longitude":"174.8433772",
+		"Elevation":"13.023"
+	},
+	25:{
+		"StationID":"25",
+		"Station":"Wingate Station",
+		"ShortCode":"WING",
+		"Latitude":"-41.18899429",
+		"Longitude":"174.9545387",
+		"Elevation":"17"
+	},
+	26:{
+		"StationID":"26",
+		"Station":"Taita Station",
+		"ShortCode":"TAIT",
+		"Latitude":"-41.18014471",
+		"Longitude":"174.9608151",
+		"Elevation":"28.121"
+	},
+	27:{
+		"StationID":"27",
+		"Station":"Pomare Station",
+		"ShortCode":"POMA",
+		"Latitude":"-41.16958985",
+		"Longitude":"174.9695591",
+		"Elevation":"29.553"
+	},
+	28:{
+		"StationID":"28",
+		"Station":"Paremata Station",
+		"ShortCode":"PARE",
+		"Latitude":"-41.10604716",
+		"Longitude":"174.8664764",
+		"Elevation":"3"
+	},
+	29:{
+		"StationID":"29",
+		"Station":"Manor Park Station",
+		"ShortCode":"MANO",
+		"Latitude":"-41.15629494",
+		"Longitude":"174.978979",
+		"Elevation":"24"
+	},
+	30:{
+		"StationID":"30",
+		"Station":"Mana Station",
+		"ShortCode":"MANA",
+		"Latitude":"-41.09515696",
+		"Longitude":"174.8681072",
+		"Elevation":"6.148"
+	},
+	31:{
+		"StationID":"31",
+		"Station":"Plimmerton Station",
+		"ShortCode":"PLIM",
+		"Latitude":"-41.08409512",
+		"Longitude":"174.8673025",
+		"Elevation":"2"
+	},
+	32:{
+		"StationID":"32",
+		"Station":"Silverstream Station",
+		"ShortCode":"SILV",
+		"Latitude":"-41.14734399",
+		"Longitude":"175.0102428",
+		"Elevation":"40.258"
+	},
+	33:{
+		"StationID":"33",
+		"Station":"Heretaunga Station",
+		"ShortCode":"HERE",
+		"Latitude":"-41.14252871",
+		"Longitude":"175.0256494",
+		"Elevation":"48"
+	},
+	34:{
+		"StationID":"34",
+		"Station":"Trentham Station",
+		"ShortCode":"TREN",
+		"Latitude":"-41.13781812",
+		"Longitude":"175.0384275",
+		"Elevation":"18"
+	},
+	35:{
+		"StationID":"35",
+		"Station":"Pukerua Bay Station",
+		"ShortCode":"PUKE",
+		"Latitude":"-41.03842919",
+		"Longitude":"174.8864535",
+		"Elevation":"80"
+	},
+	36:{
+		"StationID":"36",
+		"Station":"Wallaceville Station",
+		"ShortCode":"WALL",
+		"Latitude":"-41.13050513",
+		"Longitude":"175.0587908",
+		"Elevation":"60.778"
+	},
+	37:{
+		"StationID":"37",
+		"Station":"Upper Hutt Station",
+		"ShortCode":"UPPE",
+		"Latitude":"-41.12620584",
+		"Longitude":"175.0708178",
+		"Elevation":"51"
+	},
+	38:{
+		"StationID":"38",
+		"Station":"Maymorn Station",
+		"ShortCode":"MAYM",
+		"Latitude":"-41.10822175",
+		"Longitude":"175.1327232",
+		"Elevation":"125.517"
+	},
+	39:{
+		"StationID":"39",
+		"Station":"Paekakariki Station",
+		"ShortCode":"PAEK",
+		"Latitude":"-40.98747572",
+		"Longitude":"174.9539164",
+		"Elevation":"6.559"
+	},
+	40:{
+		"StationID":"40",
+		"Station":"Paraparaumu Station",
+		"ShortCode":"PARA",
+		"Latitude":"-40.91595474",
+		"Longitude":"175.0076134",
+		"Elevation":"12.445"
+	},
+	41:{
+		"StationID":"41",
+		"Station":"Featherston Station",
+		"ShortCode":"FEAT",
+		"Latitude":"-41.11329682",
+		"Longitude":"175.3302922",
+		"Elevation":"45.134"
+	},
+	42:{
+		"StationID":"42",
+		"Station":"Waikanae Station",
+		"ShortCode":"WAIK",
+		"Latitude":"-40.87660559",
+		"Longitude":"175.0660641",
+		"Elevation":"31.182"
+	},
+	43:{
+		"StationID":"43",
+		"Station":"Woodside Station",
+		"ShortCode":"WOOD",
+		"Latitude":"-41.06730336",
+		"Longitude":"175.4017033",
+		"Elevation":"95"
+	},
+	44:{
+		"StationID":"44",
+		"Station":"Matarawa Station",
+		"ShortCode":"MATA",
+		"Latitude":"-41.0486963",
+		"Longitude":"175.448524",
+		"Elevation":"73"
+	},
+	45:{
+		"StationID":"45",
+		"Station":"Carterton Station",
+		"ShortCode":"CART",
+		"Latitude":"-41.02205478",
+		"Longitude":"175.5232396",
+		"Elevation":"80"
+	},
+	46:{
+		"StationID":"46",
+		"Station":"Solway Station",
+		"ShortCode":"SOLW",
+		"Latitude":"-40.95370223",
+		"Longitude":"175.6248631",
+		"Elevation":"123"
+	},
+	47:{
+		"StationID":"47",
+		"Station":"Renall Street Station",
+		"ShortCode":"RENA",
+		"Latitude":"-40.94744654",
+		"Longitude":"175.6392827",
+		"Elevation":"122"
+	},
+	48:{
+		"StationID":"48",
+		"Station":"Masterton Station",
+		"ShortCode":"MAST",
+		"Latitude":"-40.94031498",
+		"Longitude":"175.6550755",
+		"Elevation":"115"
+	}
+};
